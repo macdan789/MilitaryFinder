2da833f1a6ef91ba1a8b5947337027c097678958
Bohdan Marko <markopollo789@gmail.com>
2022-07-13 17:50:31 +0300

    Add project files.

---
+++ MilitaryFinder.API/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryFinder.API.Data
+{
+    public class DbContext : IdentityDbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
