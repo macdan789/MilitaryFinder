f375ef565dc6873245d1e4e97f425783ce54d7a9
Bohdan Marko <markopollo789@gmail.com>
2022-07-13 22:10:23 +0300

    add ApiRoutes class to extract endpoint routes

---
+++ MilitaryFinder.API/Contracts/ApiRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace MilitaryFinder.API.Contracts
+{
+    public static class ApiRoutes
+    {
+        public static class FighterAircraft
+        {
+            public const string GetAll = "api/v1/aircrafts";
+            public const string Get = "api/v1/aircraft/{id}";
+            public const string Post = "api/v1/aircraft/create";
+        }
+    }
+}
